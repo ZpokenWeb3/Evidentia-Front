aab9c5c68e46de82b73d5133614e07a5f54c6587
Vadim <eugene.s@zpoken.ai>
2024-10-08 21:51:35 +0300

    delete keys

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -1,7 +1,5 @@
 import type { NextAuthOptions } from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
-//GOOGLE_CLIENT_ID=552808854470-6bvekbg5og0j28r3jdr5fm96s3csjg9f.apps.googleusercontent.com
-//GOOGLE_CLIENT_SECRET=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
 
 export const options: NextAuthOptions = {
 	providers: [
