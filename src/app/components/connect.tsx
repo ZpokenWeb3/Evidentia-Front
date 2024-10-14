8f4406cbed3f4f79eac6d65453305fb189eb1813
Vadim <eugene.s@zpoken.ai>
2024-10-14 18:53:38 +0300

    add prev address

--- src/app/components/connect.tsx
+++ src/app/components/connect.tsx
@@ -1,12 +1,12 @@
 import { ConnectButton } from 'thirdweb/react'
-import { thirdwebClient, wallets, xrpLedger } from '../config/thirdweb'
+import { accountAbstraction, thirdwebClient, wallets } from '../config/thirdweb'
 
 export const Connect = () => {
 	return (
 		<ConnectButton
 			client={thirdwebClient}
 			wallets={wallets}
-			chain={xrpLedger}
+			accountAbstraction={accountAbstraction}
 		/>
 	)
 }
--- src/app/config/erc20.ts
+++ src/app/config/erc20.ts
@@ -6,6 +6,7 @@ export const SBC: ERC20 = {
 	decimals: 6,
 	address: {
 		1440002: '0x791E5F801C2c3BA0B93Ad123b71Ee4c9b7d1b45F',
+		11155111: '0xbDBc6f32699c39DF208595BfC7Dfb96C6F837aBE',
 	},
 }
 
